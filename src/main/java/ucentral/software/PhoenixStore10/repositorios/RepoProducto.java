61c3a6222502a1f825acf3291e2e352517510649
Daniel Andrade <danielandrader23@gmail.com>
2025-04-19 12:50:46 -0500

    CU-01: Completado

---
+++ src/main/java/ucentral/software/PhoenixStore10/repositorios/RepoProducto.java
@@ -0,0 +1,15 @@
+package ucentral.software.PhoenixStore10.repositorios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ucentral.software.PhoenixStore10.entidades.Productos;
+
+import java.util.List;
+
+@Repository
+public interface RepoProducto extends JpaRepository<Productos, Long> {
+    List<Productos> findByPronombreContainingIgnoreCase(String nombre);
+
+}
+
+
